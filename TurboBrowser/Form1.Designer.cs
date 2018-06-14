b2e50668e52edad9c23bca335d876238e813e9a4
doubletee <toker.tolgahan@icloud.com>
2018-06-14 20:45:04 +0200

    Zurück Taste

--- TurboBrowser/Form1.Designer.cs
+++ TurboBrowser/Form1.Designer.cs
@@ -29,41 +29,91 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BrowserContainer));
+            this.panel1 = new System.Windows.Forms.Panel();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1654, 549);
+            this.panel1.TabIndex = 0;
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(0, 49);
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(801, 402);
+            this.webBrowser1.Size = new System.Drawing.Size(1654, 549);
             this.webBrowser1.TabIndex = 0;
-            this.webBrowser1.TabStop = false;
-            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(111, 12);
+            this.textBox1.Location = new System.Drawing.Point(199, 9);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(618, 26);
+            this.textBox1.Size = new System.Drawing.Size(1443, 26);
             this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown_1);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1654, 38);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "vor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "zurück";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
             // 
             // BrowserContainer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.webBrowser1);
+            this.ClientSize = new System.Drawing.Size(1654, 587);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "BrowserContainer";
             this.Text = "TurboBrowser v1.0";
             this.Load += new System.EventHandler(this.BrowserContainer_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,8 +121,12 @@
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- TurboBrowser/Form1.cs
+++ TurboBrowser/Form1.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TurboBrowser
 {
     public partial class BrowserContainer : Form
     {
+
+        System.IO.StreamWriter filew = new System.IO.StreamWriter("vor.txt");
+
+        string vor;
+        string urlvor;
+        string urlvor2;
+        string nach;
+        int count = 0;
+
         public BrowserContainer()
         {
             InitializeComponent();
@@ -19,7 +29,7 @@ namespace TurboBrowser
 
         private void BrowserContainer_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -38,6 +48,78 @@ namespace TurboBrowser
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                count = 1;
+                webBrowser1.Navigate(textBox1.Text);
+                vor = textBox1.Text + "<" + vor;
+                nach = "";
+                Console.WriteLine(count);
+
+
+            }
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+
+            if (count == 1)
+            {
+                Console.WriteLine(count);
+                while (vor.First() != '<')
+                {
+
+                    urlvor = urlvor + vor.ElementAt(0).ToString();
+                    vor = vor.Remove(0, 1);
+                }
+                urlvor = urlvor + vor.ElementAt(0).ToString();
+                vor = vor.Remove(0, 1);
+                nach = nach + urlvor;
+                urlvor = "";
+                while (vor.First() != '<')
+                {
+
+                    urlvor = urlvor + vor.ElementAt(0).ToString();
+                    vor = vor.Remove(0, 1);
+                }
+                webBrowser1.Navigate(urlvor);
+                urlvor = urlvor + vor.ElementAt(0).ToString();
+                vor = vor.Remove(0, 1);
+                nach = nach + urlvor;
+                urlvor = "";
+                count = 0;
+            }
+            else
+            {
+                Console.WriteLine(count+"fe");
+                while (vor.First() != '<')
+                {
+
+                    urlvor = urlvor + vor.ElementAt(0).ToString();
+                    vor = vor.Remove(0, 1);
+                }
+                webBrowser1.Navigate(urlvor);
+                urlvor = urlvor + vor.ElementAt(0).ToString();
+                vor = vor.Remove(0, 1);
+                nach = nach + urlvor;
+                urlvor = "";
+                count = 0;
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
